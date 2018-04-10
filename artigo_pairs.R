f73ea67cbead6c65c5de740df3004ec06e31ae7d
lemuelemos <lemuelemos@gmail.com>
2018-04-10 00:33:56 -0300

    New code with data from bloomberg

---
+++ artigo_pairs.R
@@ -0,0 +1,13 @@
+library(readxl)
+ibrx_2007_2018 <- read_excel("Documentos/Artigos/ibrx last price 2007 até 2018.xlsx", 
+                             sheet = "ibrx") #### Reading the data
+ibrx_2007_2018$Dates <- as.Date(ibrx_2007_2018$Dates) ## Setting the format of the dates column
+ibrx_2007_2018 <- as.data.frame(ibrx_2007_2018) ## Transform in data_frame to easy handling
+ibrx_2007_2018[,2:101] <- apply(ibrx_2007_2018[,2:101],2,as.numeric) ## Transform the data form char type to numeric type
+ibrx_2007_2018 <- xts(ibrx_2007_2018[,-1],order.by = ibrx_2007_2018$Dates) ## transform in xts to easy handling with time series
+ibrx_2007_2018_integridade <- apply(ibrx_2007_2018,2,
+                                    function(x) sum(is.na(x))/nrow(ibrx_2007_2018))*100 ## Calculate the percentage of missing data
+
+ibrx_2007_2018_70 <- ibrx_2007_2018[,names(ibrx_2007_2018_integridade[which(ibrx_2007_2018_integridade<10)])] ## Taking the coluns eith more than 90% of integrite
+ibrx_2008_2017_70 <- ibrx_2007_2018_70["2008/2017"] ## subsetting to eliminate more missing data. 
+ibrx_2008_2017_70 <- na.spline(ibrx_2008_2017_70) ## remove "NA's" spline method Missing values (NAs) are replaced by linear interpolation via approx or cubic spline interpolation via spline, respectively.
