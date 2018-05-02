57a934477734fa0f7d4b17fc29b40d89d78e5e62
lemuelemos <lemuelemos@gmail.com>
2018-05-02 00:41:11 -0300

    New script with merged codes of artigo_pairs and codigo base

---
+++ artigo_pairs_2.R
@@ -0,0 +1,352 @@
+library(readxl)
+library(xts)
+library(partialCI)
+library(stringr)
+library(dplyr)
+library(timeSeries)
+
+##### Import data and cleaning NA's
+
+ibrx_2007_2018 <- read_excel("ibrx last price 2007 até 2018.xlsx", sheet = "ibrx") #### Reading the data
+
+ibrx_2007_2018$Dates <- as.Date(ibrx_2007_2018$Dates) ## Setting the format of the dates column
+ibrx_2007_2018 <- as.data.frame(ibrx_2007_2018) ## Transform in data_frame to easy handling
+ibrx_2007_2018[,2:101] <- apply(ibrx_2007_2018[,2:101],2,as.numeric) ## Transform the data form char type to numeric type
+ibrx_2007_2018 <- xts(ibrx_2007_2018[,-1],order.by = ibrx_2007_2018$Dates) ## transform in xts to easy handling with time series
+ibrx_2007_2018_integridade <- apply(ibrx_2007_2018,2,
+                                    function(x) sum(is.na(x))/nrow(ibrx_2007_2018))*100 ## Calculate the percentage of missing data
+
+ibrx_2007_2018_70 <- ibrx_2007_2018[,names(ibrx_2007_2018_integridade[which(ibrx_2007_2018_integridade<10)])] ## Taking the coluns eith more than 90% of integrite
+ibrx_2008_2017_70 <- ibrx_2007_2018_70["2008/2017"] ## subsetting to eliminate more missing data. 
+ibrx_2008_2017_70 <- na.spline(ibrx_2008_2017_70) ## remove "NA's" spline method Missing values (NAs) are replaced by linear interpolation via approx or cubic spline interpolation via spline, respectively.
+rm(list=c("ibrx_2007_2018","ibrx_2007_2018_70")) ## remove objects that will not be use 
+Nomes <- colnames(ibrx_2008_2017_70)
+Nomes <- str_sub(Nomes, 1,6)
+colnames(ibrx_2008_2017_70) <- Nomes
+
+window_test <- seq(1,nrow(ibrx_2008_2017_70),by=132)
+ret_port <- as.list(NULL)
+
+for(p in seq_along(window_test)){
+  test_period <- window(ibrx_2008_2017_70,
+                        start=time(ibrx_2008_2017_70)[window_test[p]],
+                        end=if(is.na(time(ibrx_2008_2017_70)[window_test[p]+1007])){time(ibrx_2008_2017_70)[nrow(ibrx_2008_2017_70)]}
+                        else{time(ibrx_2008_2017_70)[window_test[p]+1007]})
+  nport <- ncol(ibrx_2008_2017_70)*(ncol(ibrx_2008_2017_70)-1)
+  ### Estimando modelo
+  portpairs <- list(NULL) ## List the will contain the pairs estimated
+  for(k in 1:ncol(test_period)){
+    for(j in 1:ncol(test_period)){
+      if(k != j){
+        portpairs[[length(portpairs)+1]] <- fit.pci(test_period[,k], test_period[,j], 
+                                                    pci_opt_method=c("jp"),
+                                                    par_model=c("par"),
+                                                    lambda=0,robust=FALSE,nu=5,include_alpha=F)
+        names(portpairs)[length(portpairs)] <- paste0(Nomes[k],"vs ",Nomes[j])
+        print(paste0("Carteira ",length(portpairs)," de ",nport,". Período de Testes ",p))
+      }
+    }
+  }
+  portpairs <- portpairs[!sapply(portpairs,is.null)] ### Retirando os valores vazios
+  portpairs <- portpairs[!sapply(portpairs, function(x) is.na(x$rho.se))] ### Retirando os pares com problemas de estimação
+  ################# Retirando os pares com o R superior a 0.5 
+  print(paste0("Retirando os pares com o R superior a 0.5. Portfólio ",p))
+  paresR <- list(NULL)
+  for(l in 1:length(portpairs)){
+    if(portpairs[[l]]$pvmr > 0.5){
+      paresR[l] <- portpairs[l]
+      names(paresR)[l] <- names(portpairs)[l]
+    }
+  }
+  paresR <- paresR[!sapply(paresR,is.null)] ### Retirando os valores vazios
+  
+  ################ Realizando o teste de significância para cointegração parcial
+  print(paste0("Realizando o teste de significância para cointegração parcial. Portfólio ",p))
+  testepci <- list(NULL)
+  paresRtested <- list(NULL)
+  for(m in 1:length(paresR)){
+    testepci[[m]] <- test.pci(paresR[[m]],alpha = 0.05, 
+                              null_hyp = c("rw", "ar1"),
+                              robust = FALSE, 
+                              pci_opt_method = c("jp"))
+    names(testepci[m]) <- names(paresR)[m]
+    print(paste0("Testando par",length(testepci)," de ",length(paresR)))
+    if(testepci[[m]]$p.value[3] <= 0.05){
+      paresRtested[m] <- paresR[m]
+      names(paresRtested)[m] <- names(paresR)[m]
+    }
+  }
+  paresRtested <- paresRtested[!sapply(paresRtested,is.null)] ### Retirando os valores vazios
+  
+  ##############################################################################
+  
+  ############# Estimando os Estados Ocultos
+  print(paste0("Estimando os Estados Ocultos. Portfólio ",p))
+  paresRtestedM <- list(NULL)
+  for(n in 1: length(paresRtested)){
+    paresRtestedM[[n]] <- statehistory.pci(paresRtested[[n]])
+    names(paresRtestedM)[n] <- names(paresRtested)[n]
+  }
+  
+  # Variável paresRtestedM já são os pares para teste backtest
+  ############### Normalizando O M
+  print(paste0("Normalizando O M. Portfólio",p))
+  Zm <- as.list(NULL)
+  for(o in 1:length(paresRtestedM)){
+    Zm[[o]] <- paresRtestedM[[o]]$M/paresRtested[[o]]$sigma_M.se
+    names(Zm)[o] <- names(paresRtestedM)[o]
+  }
+  Zm <- as.data.frame(Zm) ### Tos os M's normalizados
+  
+  ############# Sinal Para as Operações
+  ## Openright/OutRight = Operações em que o valor do resíduo é positivo
+  ## OpenLeft/OutLeft = Operações em que o valor de resídou é negativo
+  ## threshold's = [1,0.5]
+  print(paste0("Sinal Para as Operações - threshold's = [1,0.5]. Portólio ",p))
+  sinal <- data.frame(matrix(data = rep(0,ncol(Zm)*nrow(Zm)),ncol = ncol(Zm),nrow = nrow(Zm)))
+  t <- c(1,0.5)
+  sinal[1,1:length(sinal)] <- "Fora"
+  colnames(sinal) <- names(Zm)
+  for(j in 1:length(Zm)){
+    for(i in 2:nrow(Zm)){
+      if(Zm[i,j] > t[1] && sinal[i-1,j] != "OpenLeft" || sinal[i-1,j] == "OpenRight" && Zm[i,j] > -t[2]){
+        sinal[i,j] <- "OpenRight"
+      } else if(Zm[i,j] < -t[1] && sinal[i-1,j] != "OpenRight" || sinal[i-1,j] == "OpenLeft" && Zm[i,j] < t[2]){
+        sinal[i,j] <- "OpenLeft"
+      } else if(Zm[i,j] < -t[2] && sinal[i-1,j] == "OpenRight"){
+        sinal[i,j] <- "OutRight"
+      } else if(Zm[i,j] > t[2] && sinal[i-1,j] == "OpenLeft"){
+        sinal[i,j] <- "OutLeft"
+      } else{
+        sinal[i,j] <- "Fora"
+      }
+    }
+  }
+  
+  
+  
+  
+########## Loop para Pegar Preços de Entrada e Saída
+llongi <- data.frame(matrix(data = rep(0,ncol(Zm)*nrow(Zm)),ncol = ncol(Zm),nrow = nrow(Zm))) ## llongi = Left Long Inicial
+lshorti <- data.frame(matrix(data = rep(0,ncol(Zm)*nrow(Zm)),ncol = ncol(Zm),nrow = nrow(Zm))) ## lshorti = Left Short Inicial
+llongf <- data.frame(matrix(data = rep(0,ncol(Zm)*nrow(Zm)),ncol = ncol(Zm),nrow = nrow(Zm))) ## llongi = Left Long Final
+lshortf <- data.frame(matrix(data = rep(0,ncol(Zm)*nrow(Zm)),ncol = ncol(Zm),nrow = nrow(Zm)))  ## lshortf = Left Short Final
+rlongi <- data.frame(matrix(data = rep(0,ncol(Zm)*nrow(Zm)),ncol = ncol(Zm),nrow = nrow(Zm))) ## rlongi = Right Long Incial
+rshorti <- data.frame(matrix(data = rep(0,ncol(Zm)*nrow(Zm)),ncol = ncol(Zm),nrow = nrow(Zm))) ## rshorti = Right Short Incial
+rlongf <- data.frame(matrix(data = rep(0,ncol(Zm)*nrow(Zm)),ncol = ncol(Zm),nrow = nrow(Zm))) ## rlongf = Right Long Final
+rshortf <- data.frame(matrix(data = rep(0,ncol(Zm)*nrow(Zm)),ncol = ncol(Zm),nrow = nrow(Zm))) ## rshorti = Right Short Final
+tt <- data.frame(matrix(data = rep(0,ncol(Zm)*nrow(Zm)),ncol = ncol(Zm),nrow = nrow(Zm)))
+##################################################################################
+print(paste0("Coletando Preços de Entrada e Saída. Portfólio",p))
+for(j in 1:length(Zm)){
+  for(i in 2:nrow(sinal)){
+    if(sinal[i,j] == "OpenRight" 
+         && sinal[i-1,j] == "Fora"
+         && i != nrow(sinal)
+         || sinal[i,j] == "OpenRight" 
+         && sinal[i-1,j] == "OutLeft"
+         && i != nrow(sinal)
+         || sinal[i,j] == "OpenRight" 
+         && sinal[i-1,j] == "OutRight"
+         && i != nrow(sinal)){
+        rlongi[i,j] <- dados_estimacao_teste[i,grep(str_sub(names(paresRtestedM)[j],start = -8),
+                                                    colnames(dados_estimacao_teste))]
+        rshorti[i,j] <- dados_estimacao_teste[i,grep(str_sub(names(paresRtestedM)[j],end = 8),
+                                                     colnames(dados_estimacao_teste))]
+        colnames(rlongi)[j] <- paste0(names(paresRtestedM)[j],"RL")
+        colnames(rshorti)[j] <- paste0(names(paresRtestedM)[j],"RS")
+      } else if(sinal[i,j] == "OutRight" 
+                && sinal[i-1,j] == "OpenRight"
+                || i == nrow(sinal)
+                && sinal[i-1,j] == "OpenRight"){
+        rlongf[i,j] <- dados_estimacao_teste[i,grep(str_sub(names(paresRtestedM)[j],start = -8),
+                                                    colnames(dados_estimacao_teste))]
+        rshortf[i,j] <- dados_estimacao_teste[i,grep(str_sub(names(paresRtestedM)[j],end = 8),
+                                                     colnames(dados_estimacao_teste))]
+        colnames(rlongf)[j] <- paste0(names(paresRtestedM)[j],"RL")
+        colnames(rshortf)[j] <- paste0(names(paresRtestedM)[j],"RS")
+      } else if(sinal[i,j] == "OpenLeft" 
+                && sinal[i-1,j] == "Fora"
+                && i != nrow(sinal)
+                || sinal[i,j] == "OpenLeft" 
+                && sinal[i-1,j] == "OutRight"
+                && i != nrow(sinal)
+                || sinal[i,j] == "OpenLeft"
+                && sinal[i-1,j] == "OutLeft"
+                && i != nrow(sinal)){
+        llongi[i,j] <- dados_estimacao_teste[i,grep(str_sub(names(paresRtestedM)[j],end = 8),
+                                                    colnames(dados_estimacao_teste))]
+        lshorti[i,j] <- dados_estimacao_teste[i,grep(str_sub(names(paresRtestedM)[j],start = -8),
+                                                     colnames(dados_estimacao_teste))]
+        colnames(llongi)[j] <- paste0(names(paresRtestedM)[j],"LL")
+        colnames(lshorti)[j] <- paste0(names(paresRtestedM)[j],"LS")
+      } else if(sinal[i,j] == "OutLeft" 
+                && sinal[i-1,j] == "OpenLeft"
+                || i == nrow(sinal)
+                && sinal[i-1,j] == "OpenLeft"){
+        llongf[i,j] <- dados_estimacao_teste[i,grep(str_sub(names(paresRtestedM)[j],end = 8),
+                                                    colnames(dados_estimacao_teste))]
+        lshortf[i,j] <- dados_estimacao_teste[i,grep(str_sub(names(paresRtestedM)[j],start = -8),
+                                                     colnames(dados_estimacao_teste))]
+        colnames(llongf)[j] <- paste0(names(paresRtestedM)[j],"LL")
+        colnames(lshortf)[j] <- paste0(names(paresRtestedM)[j],"LS")
+      } else{
+        tt[i,j] <- sinal[i,j]
+      }
+    }
+  }
+  
+  
+  ##### Cálculo do Retorno Considerando o investimento de 1 Real.###################
+  print(paste0("Cálculo do Retorno Considerando o investimento de 1 Real. Portfólio",p))
+  invest <- data.frame(matrix(data = rep(1,ncol(Zm)*nrow(Zm)),ncol = ncol(Zm),nrow = nrow(Zm)))
+  retorno <- data.frame(matrix(data = rep(0,ncol(Zm)*nrow(Zm)),ncol = ncol(Zm),nrow = nrow(Zm)))
+  portl <- as.vector(NULL)
+  ports <- as.vector(NULL)
+  porti <- as.vector(NULL)
+  portf <- as.vector(NULL)
+  longi <- as.vector(NULL)
+  shorti <- as.vector(NULL)
+  longf <- as.vector(NULL)
+  shortf <- as.vector(NULL) 
+  ttf <- data.frame(matrix(data = rep(0,ncol(Zm)*nrow(Zm)),ncol = ncol(Zm),nrow = nrow(Zm)))
+  for(j in 1:length(sinal)){
+    for(i in 2:nrow(sinal)){
+      #invest[i,j] <- invest[k,j]
+      if(sinal[i,j] == "OpenRight" 
+         && sinal[i-1,j] == "Fora"
+         && i != nrow(sinal)
+         || sinal[i,j] == "OpenRight" 
+         && sinal[i-1,j] == "OutLeft"
+         && i != nrow(sinal)
+         || sinal[i,j] == "OpenRight" 
+         && sinal[i-1,j] == "OutRight"
+         && i != nrow(sinal)){
+        if(rlongi[i,j]*paresRtested[[j]]$beta/rshorti[i,j] < 1){
+          portl <- -((rlongi[i,j]*paresRtested[[j]]$beta*invest[i-1,j])/rshorti[i,j]) 
+          ports <- invest[i-1,j]
+          longi <- rlongi[i,j]
+          shorti <- rshorti[i,j]
+          porti <- portl+ports
+          ttf[i,j] <- "Abriu"
+          for(k in i:nrow(sinal)){
+            if(sinal[k,j] == "OutRight" 
+               && sinal[k-1,j] == "OpenRight"
+               || k == nrow(sinal)
+               && sinal[k-1,j] == "OpenRight"){
+              longf <- rlongf[k,j]
+              shortf <- rshortf[k,j]
+              longf <- ((longf/longi)-1)+1
+              shortf <- 1+((shortf/shorti)-1)
+              portf <- -portl*longf - ports*shortf
+              retorno[k,j] <- (porti+portf)/invest[k-1,j]
+              invest[k,j] <- (((porti+portf)/invest[k-1,j])+1)*invest[k-1,j]
+              ttf[k,j] <- "Saiu"
+            }
+          }
+        } else{
+          portl <- -invest[i-1,j]
+          ports <- (rshorti[i,j]/(paresRtested[[j]]$beta*rlongi[i,j]))*invest[i-1,j]
+          longi <- rlongi[i,j]
+          shorti <- rshorti[i,j]
+          porti <- portl+ports
+          ttf[i,j] <- "Abriu"
+          for(k in i:nrow(sinal)){
+            if(sinal[k,j] == "OutRight" 
+               && sinal[k-1,j] == "OpenRight"
+               || k == nrow(sinal)
+               && sinal[k-1,j] == "OpenRight"){
+              longf <- rlongf[k,j]
+              shortf <- rshortf[k,j]
+              longf <- ((longf/longi)-1)+1
+              shortf <- 1+((shortf/shorti)-1)
+              portf <- -portl*longf - ports*shortf
+              retorno[k,j] <- (porti+portf)/invest[k-1,j]
+              invest[k,j] <- (((porti+portf)/invest[k-1,j])+1)*invest[k-1,j]
+              ttf[k,j] <- "Saiu"
+            }
+          } 
+        } 
+      } else if(sinal[i,j] == "OpenLeft" 
+                && sinal[i-1,j] == "Fora"
+                && i != nrow(sinal)
+                || sinal[i,j] == "OpenLeft" 
+                && sinal[i-1,j] == "OutRight"
+                && i != nrow(sinal)
+                || sinal[i,j] == "OpenLeft"
+                && sinal[i-1,j] == "OutLeft"
+                && i != nrow(sinal)){
+        if(lshorti[i,j]*paresRtested[[j]]$beta/llongi[i,j] < 1){
+          portl <- -invest[i-1,j]
+          ports <- ((lshorti[i,j]*paresRtested[[j]]$beta)/llongi[i,j])*invest[i-1,j]
+          longi <- llongi[i,j]
+          shorti <- lshorti[i,j]
+          porti <- portl+ports
+          ttf[i,j] <- "Abriu"
+          for(k in i:nrow(sinal)){
+            if(sinal[k,j] == "OutLeft" 
+               && sinal[k-1,j] == "OpenLeft"
+               || k == nrow(sinal)
+               && sinal[k-1,j] == "OpenLeft"){
+              longf <- llongf[k,j]
+              shortf <- lshortf[k,j]
+              longf <- ((longf/longi)-1)+1
+              shortf <- 1+((shortf/shorti)-1)
+              portf <- -portl*longf - ports*shortf
+              retorno[k,j] <- (porti+portf)/invest[k-1,j]
+              invest[k,j] <- (((porti+portf)/invest[k-1,j])+1)*invest[k-1,j]
+              ttf[k,j] <- "Saiu"
+            }
+          } 
+        } else{
+          portl <- -(llongi[i,j]/(paresRtested[[j]]$beta*lshorti[i,j]))*invest[i-1,j]
+          ports <- invest[i-1,j]
+          longi <- llongi[i,j]
+          shorti <- lshorti[i,j]
+          porti <- portl+ports
+          ttf[i,j] <- "Abriu"
+          for(k in i:nrow(sinal)){
+            if(sinal[k,j] == "OutLeft" 
+               && sinal[k-1,j] == "OpenLeft"
+               || k == nrow(sinal)
+               && sinal[k-1,j] == "OpenLeft"){
+              longf <- llongf[k,j]
+              shortf <- lshortf[k,j]
+              longf <- ((longf/longi)-1)+1
+              shortf <- 1+((shortf/shorti)-1)
+              portf <- -portl*longf - ports*shortf
+              retorno[k,j] <- (porti+portf)/invest[k-1,j]
+              invest[k,j] <- (((porti+portf)/invest[k-1,j])+1)*invest[k-1,j]
+              ttf[k,j] <- "Saiu"
+            }
+          } 
+        } 
+      } else{
+        if(i == k){          ### Mudar teste de condição. Problemático pelo o k só ser declarado dentro do laço for. 
+          ttf[i,j] <- "Saiu" ### Tentar sinal[i,j] == sina[i-1,j], então ttf[i,j] <- "Aberto", senão, ttf[i,j] <- "Saiu"
+        } else{
+          ttf[i,j] <- "Aberto"
+        }
+      }
+    }
+  }
+  
+  names(invest) <- names(paresRtested) ### Nomeando os Pares
+  names(retorno) <- names(paresRtested)
+  
+  ################ Cáculo dos Retornos Totais, Desvios Padrões e Sharpe.
+  print(paste0("Calculando os Retornos Totais. Portfólio ",p))
+  portret <- as.data.frame(matrix(data = rep(0,ncol(Zm)*3),ncol = ncol(Zm),nrow = 3))
+  for(f in 1:length(invest)){
+    portret[1,f] <- (cumprod(invest[,f])[nrow(invest)]-1)*100
+    portret[2,f] <- sd(cumprod(invest[,f]))
+    portret[3,f] <- portret[1,f]/portret[2,f]
+    colnames(portret)[f] <- names(paresRtestedM)[f]
+  }
+  
+  portret <- t(portret) ## Retornos Totais
+  colnames(portret) <- c("Retorno Total","Desvio Padrão","Sharpe")
+  ret_port[[length(ret_port)+1]] <- portret ## Retornos Totais
+  names(ret_port)[p] <- paste0("Return Formation Period ",p)
+}
+
