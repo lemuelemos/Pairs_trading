c8e5dfd506afcb0659cb5dbeef353fc8ecbc0efb
lemuelemos <lemuelemos@gmail.com>
2018-05-28 15:31:31 -0300

    Little corrections

--- res_data_est.R
+++ res_data_est.R
@@ -6,9 +6,6 @@ resum_results[[2]] <- t(sapply(trading_return, function(x) x[[2]] %>% apply(.,2,
 names(resum_results)[1] <- "Top 20 Sharp"
 names(resum_results)[2] <- "Top 20 Retur"
 
-resum_results[[1]] <- t(sapply(trading_return, function(x) x[[1]] %>% apply(.,2,mean)))
-resum_results[[2]] <- t(sapply(trading_return, function(x) x[[2]] %>% apply(.,2,mean)))
-
 r_names_sharp <- str_sub(sapply(trading_return, function(x) names(x)[1]), end = -19)
 r_names_retur <- str_sub(sapply(trading_return, function(x) names(x)[2]), end = -20)
 rownames(resum_results[[1]]) <- r_names_retur
@@ -19,7 +16,7 @@ par_estim <- list(NULL)
 par_estim[[1]] <- resum_results
 par_estim[[2]] <- trading_return
 par_estim[[3]] <- select_port
-saveRDS(par_estim,paste0("par_estim threshold[",tr[1],",",tr[2],"].rds"))
+saveRDS(par_estim,paste0("par_estim threshold[",tr[1],",",tr[2],"]_,",names(formation_windown)[pp],".rds"))
 
 #resum_results
 
