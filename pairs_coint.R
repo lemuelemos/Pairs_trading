4d73bb04036b20727d86913b34f0cebe154ffd72
lemuelemos <lemuelemos@gmail.com>
2018-06-19 13:36:22 -0300

    Coint_pairs

---
+++ pairs_coint.R
@@ -0,0 +1,59 @@
+library(doParallel)
+library(plyr)
+library(readxl)
+library(xts)
+library(stringr)
+library(dplyr)
+library(timeSeries)
+library(Rcpp)
+
+ibrx_2007_2018 <- read_excel("ibrx last price 2007 até 2018_2.xlsx") #### Reading the data
+
+ibrx_2007_2018$Dates <- as.Date(ibrx_2007_2018$Dates) ## Setting the format of the dates column
+ibrx_2007_2018 <- as.data.frame(ibrx_2007_2018) ## Transform in data_frame to easy handling
+ibrx_2007_2018[,2:ncol(ibrx_2007_2018)] <- apply(ibrx_2007_2018[,2:ncol(ibrx_2007_2018)],2,as.numeric) ## Transform the data form char type to numeric type
+ibrx_2007_2018 <- xts(ibrx_2007_2018[,-1],order.by = ibrx_2007_2018$Dates) ## transform in xts to easy handling with time series
+ibrx_2007_2018_integridade <- apply(ibrx_2007_2018,2,
+                                    function(x) sum(is.na(x))/nrow(ibrx_2007_2018))*100 ## Calculate the percentage of missing data
+
+ibrx_2007_2018_70 <- ibrx_2007_2018[,names(ibrx_2007_2018_integridade[which(ibrx_2007_2018_integridade<10)])] ## Taking the coluns eith more than 90% of integrite
+ibrx_2008_2017_70 <- ibrx_2007_2018_70["2008/2017"] ## subsetting to eliminate more missing data. 
+ibrx_2008_2017_70 <- na.spline(ibrx_2008_2017_70) ## remove "NA's" spline method Missing values (NAs) are replaced by linear interpolation via approx or cubic spline interpolation via spline, respectively.
+rm(list=c("ibrx_2007_2018","ibrx_2007_2018_70")) ## remove objects that will not be use 
+Nomes <- colnames(ibrx_2008_2017_70) ## Taking the names of equity's
+Nomes <- str_sub(Nomes, 1,6)
+colnames(ibrx_2008_2017_70) <- Nomes
+
+
+window_test <- seq(1,nrow(ibrx_2008_2017_70),by=126)
+ret_port <- as.list(NULL)
+trading_return <- as.list(NULL)
+select_port <- as.list(NULL)
+retornos <- as.list(NULL)
+time_window <- as.list(NULL)
+ret_aux <- as.list(NULL)
+threshold <- matrix(c(1,1.5,1,1.5,0.5,0.5,1,1),4,2)
+formation_windown <- c(251,503,1007)
+names(formation_windown) <- c("1Y","2Y","4Y")
+for(pp in 1){
+    for(p in 1){
+      test_period <- window(ibrx_2008_2017_70,
+                            start=time(ibrx_2008_2017_70)[window_test[p]],
+                            end=if(is.na(time(ibrx_2008_2017_70)[window_test[p]+formation_windown[pp]])){break}
+                            else{time(ibrx_2008_2017_70)[window_test[p]+formation_windown[pp]]})
+      time_window[[p]] <- time(test_period)
+      test_period_est <- as.data.frame(test_period)
+      ### Estimating pairs
+      print(paste0("Estimating the pairs from portfolio "
+                   ,p,". Period from ",
+                   min(time_window[[p]]), " to ",max(time_window[[p]])))
+      
+      no_cores <- detectCores() 
+      cl <- makeCluster(no_cores)
+      clusterExport(cl, "test_period_est")
+      clusterEvalQ(cl, library(partialCI))
+      pares <- parLapply(cl,test_period_est,function(x) apply(test_period_est,2, 
+                                                              function(y) if(x!=y){fit.pci(x,y)}))
+      stopCluster(cl)
+    }
+}
